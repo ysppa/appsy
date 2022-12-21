0c067838c7987c5393f395a5fdc96f0d29614bac
plinsy <plinsy2@gmail.com>
2022-12-21 14:21:53 +0300

    Updates

---
+++ precache-manifest.8f2268b8c9a6ff4ee408253ca676b619.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "13c6929375f3801e7d4f",
+    "url": "/appsy/static/css/main.261f5bc2.chunk.css"
+  },
+  {
+    "revision": "13c6929375f3801e7d4f",
+    "url": "/appsy/static/js/main.13c69293.chunk.js"
+  },
+  {
+    "revision": "dcb7df9bbfde9dd1c6b0",
+    "url": "/appsy/static/js/1.dcb7df9b.chunk.js"
+  },
+  {
+    "revision": "f4501e54ed09e2b9cd57",
+    "url": "/appsy/static/css/2.96bad554.chunk.css"
+  },
+  {
+    "revision": "f4501e54ed09e2b9cd57",
+    "url": "/appsy/static/js/2.f4501e54.chunk.js"
+  },
+  {
+    "revision": "f4106861366ec8d4f403",
+    "url": "/appsy/static/js/runtime~main.f4106861.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/appsy/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "7711436adcd70d3e4e1b7215e0fd2a06",
+    "url": "/appsy/index.html"
+  }
+];
